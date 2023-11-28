9686aae44bb83368b11a3e1f5ab2d1bd4cb128bb
Lina <41321441+Linamcaro@users.noreply.github.com>
2023-11-28 00:11:05 -0500

    UI update

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
@@ -28,7 +30,10 @@ public class GameUI : MonoBehaviour
     }
 
 
-    
+    public void ReturnMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 
 
     private void Show()
